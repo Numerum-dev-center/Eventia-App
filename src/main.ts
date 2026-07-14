142a54d48bd0d93dffbf984c673b8b5062df3324
Riccardo-67 <samabotchoessodjolo@gmail.com>
2026-07-14 15:11:53 +0000

    Config Swagger

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  // --- Configuration Swagger ---
+  const config = new DocumentBuilder()
+    .setTitle('API de Eventia')
+    .setDescription('La documentation de notre  API')
+    .setVersion('1.0')
+    .addTag('users') // Optionnel : pour organiser par tags
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // L'UI sera accessible sur /api-docs
+  // -----------------------------
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true,
       forbidNonWhitelisted: true,
