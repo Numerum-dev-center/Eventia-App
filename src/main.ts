1484137d1d6fd9e214da2e9ed3548a2a50e59d00
Riccardo-67 <samabotchoessodjolo@gmail.com>
2026-07-14 15:28:22 +0000

    Correction main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,38 +11,39 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
 
-  // --- Configuration Swagger ---
-  const config = new DocumentBuilder()
-    .setTitle('API de Eventia')
-    .setDescription('La documentation de notre  API')
-    .setVersion('1.0')
-    .addTag('users') // Optionnel : pour organiser par tags
-    .build();
-    
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document); // L'UI sera accessible sur /api-docs
-  // -----------------------------
-
-  app.useGlobalPipes(
-    new ValidationPipe({ whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,  }),
-  );
-
-  async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieParser()); // Active la lecture des cookies
-  await app.listen(3000);
-}
-
+  // 1. Configuration des Middlewares
+  app.use(cookieParser());
+  
   app.enableCors({
     origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:3000'], 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Nécessaire si tu utilises des cookies ou sessions
+    credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
-    exposedHeaders: 'Authorization', // Utile si tu exposes des headers custom
+    exposedHeaders: 'Authorization',
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({ 
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,  
+    }),
+  );
+
+  // 2. Configuration Swagger (AVANT le listen)
+  const config = new DocumentBuilder()
+    .setTitle('API de Eventia')
+    .setDescription('La documentation de notre API')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+    
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  // 3. Démarrage du serveur 
+  await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
