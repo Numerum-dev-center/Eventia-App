8b2e189c6ca8d220dbe202bf6bcb3d58e3353c29
Riccardo-67 <samabotchoessodjolo@gmail.com>
2026-07-21 12:09:28 +0000

    utilisateur module

--- src/utilisateur/utilisateur.module.ts
+++ src/utilisateur/utilisateur.module.ts
@@ -10,6 +10,8 @@ import { TicketEmis } from 'src/tickets/entities/ticket-emis.entity';
 import { SessionsJetons } from 'src/auth/sessions-jetons/entities/sessions-jeton.entity';
 import { LogsAudit } from 'src/logs-audit/entities/logs-audit.entity';
 import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 @Module({
   imports: [
     forwardRef(() => AuthModule),
@@ -21,6 +23,14 @@ import { AuthModule } from 'src/auth/auth.module';
       SessionsJetons,
       LogsAudit,
     ]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '24h' },
+      }),
+    }),
     MailModule,
   ],
   controllers: [UtilisateurController],
