9ac0ce86cf83bc1a2feab3b905530ef088ecafd3
Riccardo-67 <samabotchoessodjolo@gmail.com>
2026-07-21 17:11:33 +0000

    fix auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -98,7 +98,7 @@ export class AuthController {
   // 2. Génération de la paire de tokens
   const { accessToken, refreshToken } = await this.authService.generateTokens(user, req);
   
-  const frontendUrl = this.configService.get<string>('FRONTEND_URL');
+  const frontendUrl = this.configService.get<string>('FRONTEND_URL_PROD');
   // 3. Redirection vers le Front avec les tokens
   // On passe les deux tokens en query params
   res.redirect(
