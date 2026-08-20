98adb2f2108bb065f9f928c637d9e130bad329c8
Godson Patam <patgodson01@gmail.com>
2026-08-20 10:51:01 +0000

    feat: open CORS to all origins (*)

--- Eventia-App-dev/src/main.ts
+++ Eventia-App-dev/src/main.ts
@@ -18,9 +18,7 @@ async function bootstrap() {
   app.use(cookieParser());
   
   app.enableCors({
-    origin: process.env.CORS_ORIGIN
-      ? process.env.CORS_ORIGIN.split(',')
-      : ['https://eventia-beige.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
